be1401d2dc7eaecdfdd6ce5a5f853a958cab57f7
Jaiprakash kushwaha <jaiprakash.kushwaha@cogoport.com>
2023-08-28 09:11:32 +0530

    to-do

--- to-do-app/src/to-do/TodoForm/index.js
+++ to-do-app/src/to-do/TodoForm/index.js
@@ -1,15 +1,32 @@
+import React from "react";
 
-const TodoForm=({todoItem='',setTodoItem=()=>{}, handleKeyDown=()=>{}, handleAddItem=()=>{}})=>{
+const TodoForm = ({
+    todoItem = "",
+    setTodoItem = () => {},
+    handleKeyDown = () => {},
+    handleAddItem = () => {},
+}) => {
+    const isValidInput = todoItem.trim() === "";
 
-    const isValidInput = todoItem.trim()==='';
-
-    return(
+    return (
         <div className="container">
-        <form >
-            <input type="text" value={todoItem} onChange={(e)=>setTodoItem(e.target.value)} onKeyDown={handleKeyDown}/>
-        </form>
-        <button className='button' type="submit" onClick={handleAddItem} disabled={isValidInput}>Add</button>
-    </div>
-    )
-}
+            <form>
+                <input
+                    type="text"
+                    value={todoItem}
+                    onChange={(e) => setTodoItem(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                />
+            </form>
+            <button
+                className="button"
+                type="submit"
+                onClick={handleAddItem}
+                disabled={isValidInput}
+            >
+                Add
+            </button>
+        </div>
+    );
+};
 export default TodoForm;
